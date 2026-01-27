ad8889cffb9a8e2235fa169ee759461fcfb2355f
Troy Johnston <troy@cloud-team.com>
2026-01-27 11:47:18 -0500

    Add default values to injection tokens

--- projects/ccc-lib/types/tokens.ts
+++ projects/ccc-lib/types/tokens.ts
@@ -5,25 +5,26 @@ import { MethodMeta, ResourceMeta } from './resource-meta';
 /**
  * The base URL for API requests (e.g., 'https://api.example.com/').
  */
-export const BASE_URL = new InjectionToken<string>('BASE_URL');
+export const BASE_URL = new InjectionToken<string>('BASE_URL', { factory: () => '/' });
 
 /**
  * The path to the frontend login page (e.g., '/login').
  */
-export const FRONTEND_LOGIN_PATH = new InjectionToken<string>('FRONTEND_LOGIN_PATH');
+export const FRONTEND_LOGIN_PATH = new InjectionToken<string>('FRONTEND_LOGIN_PATH', { factory: () => '/login' });
 
 /**
  * The path to the session endpoint (e.g., 'user/session').
  */
-export const SESSION_PATH = new InjectionToken<string>('SESSION_PATH');
+export const SESSION_PATH = new InjectionToken<string>('SESSION_PATH', { factory: () => 'user/session' });
 
 /**
  * The base URL for API requests (e.g., '/api').
  */
-export const API_URL = new InjectionToken<string>('API_URL');
+export const API_URL = new InjectionToken<string>('API_URL', { factory: () => '/api' });
 
 export const PERMISSION_REQUIRED = new InjectionToken<(resource: Resource, permission: Permission) => boolean>(
   'PERMISSION_REQUIRED',
+  { factory: () => () => false },
 );
 
 export const AVAILABLE_PERMISSIONS = new InjectionToken<{
@@ -33,41 +34,49 @@ export const AVAILABLE_PERMISSIONS = new InjectionToken<{
   Read: Permission;
   Update: Permission;
 }>('AVAILABLE_PERMISSIONS');
-export const AVAILABLE_DOMAINS = new InjectionToken<Record<string, Domain>[]>('AVAILABLE_DOMAINS');
+export const AVAILABLE_DOMAINS = new InjectionToken<Record<string, Domain>[]>('AVAILABLE_DOMAINS', {
+  factory: () => [],
+});
 
 /**
  * The generated resourceMeta from the output of the [ccc package](https://github.com/cccteam/ccc) typescript generator
  * Required to work with generated routes and resource metadata
  */
-export const RESOURCE_META = new InjectionToken<(resource: Resource) => ResourceMeta>('RESOURCE_META');
+export const RESOURCE_META = new InjectionToken<(resource: Resource) => ResourceMeta>('RESOURCE_META', {
+  factory: () => () => ({}) as ResourceMeta,
+});
 
 /**
  * The generated methodMeta from the output of the [ccc package](https://github.com/cccteam/ccc) typescript generator
  * Required to work with generated method data
  */
-export const METHOD_META = new InjectionToken<(method: string) => MethodMeta>('METHOD_META');
+export const METHOD_META = new InjectionToken<(method: string) => MethodMeta>('METHOD_META', {
+  factory: () => () => ({}) as MethodMeta,
+});
 
 /**
  * The duration in seconds for the session to be considered idle.
  */
-export const IDLE_SESSION_DURATION = new InjectionToken<number>('IDLE_SESSION_DURATION');
+export const IDLE_SESSION_DURATION = new InjectionToken<number>('IDLE_SESSION_DURATION', { factory: () => 300 });
 
 /**
  * The duration in seconds for the warning to be shown to the user before the session times out due to inactivity.
  */
-export const IDLE_WARNING_DURATION = new InjectionToken<number>('IDLE_WARNING_DURATION');
+export const IDLE_WARNING_DURATION = new InjectionToken<number>('IDLE_WARNING_DURATION', { factory: () => 60 });
 
 /**
  * The duration in seconds for the keepalive ping to be sent to the server to keep the session alive.
  */
-export const IDLE_KEEPALIVE_DURATION = new InjectionToken<number>('IDLE_KEEPALIVE_DURATION');
+export const IDLE_KEEPALIVE_DURATION = new InjectionToken<number>('IDLE_KEEPALIVE_DURATION', { factory: () => 30 });
 
 /**
  * A function to be called when the user logs out.
  */
-export const LOGOUT_ACTION = new InjectionToken<() => void>('LOGOUT_ACTION');
+export const LOGOUT_ACTION = new InjectionToken<() => void>('LOGOUT_ACTION', { factory: () => () => 0 });
 
 /**
  * A function to be called when the user is logged out due to inactivity.
  */
-export const IDLE_LOGOUT_ACTION = new InjectionToken<() => void>('IDLE_LOGOUT_ACTION');
+export const IDLE_LOGOUT_ACTION = new InjectionToken<() => void>('IDLE_LOGOUT_ACTION', {
+  factory: () => () => 0,
+});
